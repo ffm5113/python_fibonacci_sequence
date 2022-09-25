32bb8d329ea0cedfa8a2c9834c006c31da2e0928
ffm5113 <73726304+ffm5113@users.noreply.github.com>
2022-09-24 17:48:29 -0700

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -24,3 +24,7 @@ def main():
     fibonacci.integers()           
     
 if __name__ == '__main__': main()
+
+# OUTPUT
+# Fibonacci showed us that humans have a remarkable ability to identify patterns:
+# 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765
